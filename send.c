249f27c209bd47a9adbecfc2a367e9c9cda78e2d
Ömer Faruk İlhan <omerilhan2452@gmail.com>
2024-05-18 22:39:43 +0300

    getting from_addr info

--- send.c
+++ send.c
@@ -23,14 +23,16 @@ int main() {
         return 1;
     }
     SOCKET server_sockfd;
-    struct sockaddr_in server_addr;
+    struct sockaddr_in server_addr,from_addr;
+    socklen_t fromlen = sizeof(from_addr);
+
 
     printf("command IP path\n");
     scanf_s("%s %s %s", command,32, ip,16, path,64);
    
     if (strcmp(command,"send") == 0 && ip != '\0' && path != NULL) {
         server_sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-
+        //verdiðin pathteki dosyayý aç
         FILE* sendFile = fopen(path,"r");
 
         if (server_sockfd == INVALID_SOCKET) {
@@ -58,7 +60,7 @@ int main() {
 
         int bytes_sent;
         while (fgets(buffer, SIZE, sendFile) != NULL) {
-            printf("Sending Data: %s\N", buffer);
+            printf("Sending Data: %s\n", buffer);
 
             //server_socketi aracýlýðýyla - server addresine buffer'ý gönder
             bytes_sent = sendto(server_sockfd, buffer, (int)strlen(buffer), 0, (struct sockaddr*)&server_addr, sizeof(server_addr));
@@ -68,25 +70,27 @@ int main() {
                 WSACleanup();
                 return 1;
             }
-            printf("\nData sent: %d bytes", bytes_sent);
-
-            bytes_recv = recvfrom(server_sockfd,recvBuf,bytes_sent,0,NULL,NULL);
+            printf("Data sent: %d bytes\n", bytes_sent);
+            //Eðer datanýn nereden geldiði ile ilgili bilgiyi tutmak istiyorsan from_addr ve fromlen parametresini kullan
+            //Else -> NULL
+            bytes_recv = recvfrom(server_sockfd,recvBuf,bytes_sent,0,&from_addr,&fromlen);
             if (bytes_recv < 0) {
                 printf("RECV Error\n");
                 closesocket(server_sockfd);
                 return 1;
 
             }
+            //File'ý append modunda açma iþlemini yap - eðer file yoksa recv.txt oluþtur
             FILE* recvFile = fopen("recv.txt", "a");
             if (recvFile == NULL) {
                 printf("Failed to create file\n");
                 return 1;
             }
+            //null terminate
             recvBuf[bytes_recv] = '\0';
 
             fwrite(recvBuf, 1, (size_t)strlen(recvBuf), recvFile);
            
-
             //her buffer'da SIZE kadar elemaný sýfýrla
             memset(buffer, 0, SIZE);
         }
